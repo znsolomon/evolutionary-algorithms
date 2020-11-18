f42b2cea8d7d574543a3859c3bf22ed2218454b6
znsolomon <znsolomon@gmail.com>
2020-11-18 10:52:17 +0000

    Convert support function to python

--- recursive_parento_shell_with_duplicates.py
+++ recursive_parento_shell_with_duplicates.py
@@ -1,34 +1,32 @@
-function [shell] = recursive_pareto_shell_with_duplicates(Y,index)
+import numpy as np
 
-% function [shell] = recursive_pareto_shell(Y,index)
-%
-% function recusively computes Pareto shell membership
-%
-% Y = A n by m matrix of objectives, where m is the number of objectives
-% and n is the number of points
-% index = the integer value you wish to attribute to the estimated Pareto
-% optimal shell (typically 0 or 1)
-%
-% shell = n by 1 array of corresponding shell membership values
-%
-% copes with duplicates
-% assumes minimisation
-%
-% Jonathan Fieldsend, 2011, University of Exeter
+def recursive_pareto_shell_with_duplicates(Y,index):
 
-[n,m] = size(Y);
-S = zeros(n,1);
-shell = zeros(n,1);
+    # function [shell] = recursive_pareto_shell(Y,index)
 
-for i=1:n
-    % get number of points that dominate Y
-    S(i) = sum((sum(Y<=repmat(Y(i,:),n,1),2) == m) & (sum(Y<repmat(Y(i,:),n,1),2) > 0));
-end
-S=S+1;
-shell(S==1)=index;
+    # function recursively computes Pareto shell membership
 
-if sum(S)==n % if at last shell, terminate and chain back
-    return
-else
-    shell(S~=1) = recursive_pareto_shell_with_duplicates(Y(S~=1,:),index+1);
-end
+    # Y = A n by m matrix of objectives, where m is the number of objectives
+    # and n is the number of points
+    # index = the integer value you wish to attribute to the estimated Pareto
+    # optimal shell (typically 0 or 1)
+
+    # shell = n by 1 array of corresponding shell membership values
+
+    # copes with duplicates
+    # assumes minimisation
+
+    [n, m] = Y.shape
+    S = np.zeros(n, 1)
+    shell = np.zeros(n, 1)
+
+    for i in range(n):
+        # get number of points that dominate Y
+        S[i] = sum((sum(Y <= np.tile(Y[i, :], (n, 1)), 2) == m) & (sum(Y < np.tile(Y[i, :], n, 1), 2) > 0))
+    S = S+1
+    shell[S == 1] = index
+
+    if sum(S) == n:  # if at last shell, terminate and chain back
+        return shell
+    else:
+        shell[S != 1] = recursive_pareto_shell_with_duplicates(Y[S != 1, :], index+1)
